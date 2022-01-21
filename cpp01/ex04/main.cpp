e9b72e1faf0fe82b7a505caf4759f88150a5ee2a
inshin <inshin@student.42seoul.kr>
2022-01-22 05:18:21 +0900

    FIX: error condition

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,12 +6,9 @@
 /*   By: inshin <inshin@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 02:57:43 by inshin            #+#    #+#             */
-/*   Updated: 2022/01/22 05:05:16 by inshin           ###   ########seoul.kr  */
+/*   Updated: 2022/01/22 05:17:44 by inshin           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
-
-// 파일 열고, 파일 내의 모든 s1을 s2로 대체한 후 FILENAME.replace 에 저장
-// insert, erase
 
 #include <iostream>
 #include <string>
@@ -27,6 +24,11 @@ int main(int ac, char **av)
 	std::string fileName = av[1];
 	std::string str1 = av[2];
 	std::string str2 = av[3];
+	if (fileName.empty() || str1.empty() || str2.empty())
+	{
+		std::cout << "Empty arguments" << std::endl;
+		return 0;
+	}
 	std::ifstream openFile(fileName);
 	std::string extension = ".replace";
 	if (openFile.is_open())
