911356d63a8ffe3f93d20bde5dea161ee01dc233
inshin <inshin@student.42seoul.kr>
2022-02-10 13:48:48 +0900

    fix cpp06 ex01 main.cpp

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,23 +6,29 @@
 /*   By: inshin <inshin@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 02:07:43 by inshin            #+#    #+#             */
-/*   Updated: 2022/02/10 02:58:20 by inshin           ###   ########seoul.kr  */
+/*   Updated: 2022/02/10 13:47:57 by inshin           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iomanip>
 #include "Data.hpp"
 
 int main(void)
 {
-	Data id = {"inshin", 33, 77};
-	std::cout << "Data addr: " << &id << std::endl;
+	Data id;
+
+	id.name = "inshin";
+	id.age =  33;
+	id.weight = 77;
+
+	std::cout << std::setw(30) << "Data ptr addr: " << &id << std::endl;
 
 	uintptr_t serial = serialize(&id);
-	std::cout << "serialized decimal ptr addr: " << serial << std::endl;
-	std::cout << "serialized hex ptr addr: "<< std::hex << serial << std::dec << std::endl;
+	std::cout << std::setw(30) << "serialized decimal ptr addr: " << serial << std::endl;
+	std::cout << std::setw(30) << "serialized hex ptr addr: "<< std::hex << serial << std::dec << std::endl;
 
 	Data *deserial = deserialize(serial);
-	std::cout << "deserialized ptr addr: "  << deserial << std::endl;
+	std::cout << std::setw(30) << "deserialized ptr addr: "  << deserial << std::endl;
 
 	std::cout << "id  : " << deserial->name << std::endl;
 	std::cout << "age : " << deserial->age << std::endl;
