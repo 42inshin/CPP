9c7a66023bf1397a20d00de0fc472eb2fd78665a
inshin <inshin@student.42seoul.kr>
2022-02-10 15:38:04 +0900

    ex01

--- cpp06/ex01/Data.hpp
+++ cpp06/ex01/Data.hpp
@@ -6,7 +6,7 @@
 /*   By: inshin <inshin@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 02:24:37 by inshin            #+#    #+#             */
-/*   Updated: 2022/02/10 05:34:40 by inshin           ###   ########seoul.kr  */
+/*   Updated: 2022/02/10 15:37:06 by inshin           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 #include <iostream>
 #include <string>
-#include <cstdint> // uintptr_t
+
 // uintptr_t: unsigned integer pointer type
 // 포인터 주소를 변환하고 다시 원래 포인터와 동일한 값으로 변환할 수 있는 정수형 타입이다.
 // 해당 타입으로 변환 시 캐스팅이 필요하다.
