5a9c354fc6fc878c66d96ace76c48cf0cee6b18d
inshin <inshin@student.42seoul.kr>
2022-02-11 22:21:03 +0900

    cpp08 ex00 fix: std::find()

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: inshin <inshin@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 17:51:48 by inshin            #+#    #+#             */
-/*   Updated: 2022/02/11 22:18:44 by inshin           ###   ########seoul.kr  */
+/*   Updated: 2022/02/11 22:20:43 by inshin           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ typename T::iterator easyfind(T& c, int n)
 		typename T::iterator it;
 
 		it = std::find(c.begin(), c.end(), n);
-		if (it == std::end(c))
+		if (it == c.end())
 			throw std::runtime_error("It can't be found");
 		return it;
 }
